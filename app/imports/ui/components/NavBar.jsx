3a9c65ced084daced6247a99b02876f0f5d76074
Connor Sonoda <43054889+csonoda@users.noreply.github.com>
2022-11-13 20:24:13 -1000

    Edited Navbar Component - Added in Buttons for different pages
    Did not create drop-downs as there is no guidance for pages yet.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -16,18 +16,23 @@ const NavBar = () => {
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
-          <h2>meteor-application-template-react</h2>
+          <h2>UH Campus Tour</h2>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-start">
+          <Nav className="me-auto justify-content-evenly">
+            <Nav.Link id="about-uhm-nav" as={NavLink} to="/about" key="about">About UHM</Nav.Link>
+            <Nav.Link id="student-life-nav" as={NavLink} to="/life" key="life">Student Life</Nav.Link>
+            <Nav.Link id="department-nav" as={NavLink} to="/department" key="department">Department</Nav.Link>
+            <Nav.Link id="interest-nav" as={NavLink} to="/interest" key="interest">Interest</Nav.Link>
             {currentUser ? ([
-              <Nav.Link id="add-stuff-nav" as={NavLink} to="/add" key="add">Add Stuff</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/list" key="list">List Stuff</Nav.Link>,
+              <Nav.Link id="my-interests-nav" as={NavLink} to="/myinterests" key="myinterests">My Interests</Nav.Link>,
             ]) : ''}
-            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
-            ) : ''}
+            {Roles.userIsInRole(Meteor.userId(), 'admin') ? ([
+              <Nav.Link id="department-Name-admin-nav" as={NavLink} to="/departmentName" key="departmentName">departmentName</Nav.Link>,
+              <Nav.Link id="statistics-admin-nav" as={NavLink} to="/stats" key="stats">Statistics</Nav.Link>,
+              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Administration</Nav.Link>,
+            ]) : ''}
           </Nav>
           <Nav className="justify-content-end">
             {currentUser === '' ? (
