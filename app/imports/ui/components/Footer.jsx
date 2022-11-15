f790a1f1c8ae6bfae19b8022562d12db57fb96d7
amandanitta <amandanitta458@gmail.com>
2022-11-15 07:36:59 -1000

    Color working - footer stretches across whole scrreen

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Image } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
@@ -8,22 +8,27 @@ const Footer = () => (
       <h2 className="take-me-to-manoa">#TakeMeToManoa</h2>
       Improving lives through education and research
     </Container>
-    <Container className="footer-color">
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-          Template Home
-          Page
-        </a>
-      </Col>
-    </Container>
+    <div className="footer-color">
+      <Container>
+        <Col className="text-left">
+          <Image src="https://manoa.hawaii.edu/wp/wp-content/uploads/2017/10/uhm-white-seal-nameplate@2x.png" />
+        </Col>
+        <Col className="text-center">
+          Department of Information and Computer Sciences
+          {' '}
+          <br />
+          University of Hawaii
+          <br />
+          Honolulu, HI 96822
+          {' '}
+          <br />
+          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
+            Template Home
+            Page
+          </a>
+        </Col>
+      </Container>
+    </div>
   </footer>
 );
 
