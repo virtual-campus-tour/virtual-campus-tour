f4cd9ed63cd863fcb440a95b9903c1e5f68cb9a5
Derek Nishimura <nishimuradj@gmail.com>
2022-11-20 21:14:00 -1000

    Added boxes for elements

--- app/imports/ui/pages/Department.jsx
+++ app/imports/ui/pages/Department.jsx
@@ -1,19 +1,56 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Department = () => (
 
   <>
     <Container>
-      <h2 className="text-center">Header</h2>
+      <h2 className="text-center">Information and Computer Science</h2>
+      <Container className="box-size">
+        <h3>Image Box</h3>
+      </Container>
     </Container>
     <hr className="line-thick" />
-    <Container className="dept-spacer">
+    <Container>
       <h2 className="text-center">Clubs</h2>
 
+      <Row>
+        <Col>
+          <Container className="container-border">
+            <p>Text Field</p>
+          </Container>
+        </Col>
+        <Col>
+          <Container className="container-border">
+            <p>Text Field</p>
+          </Container>
+        </Col>
+        <Col>
+          <Container className="container-border">
+            <p>Text Field</p>
+          </Container>
+        </Col>
+      </Row>
     </Container>
     <Container className="dept-spacer">
       <h2 className="text-center">Research</h2>
+      <Row>
+        <Col>
+          <Container className="container-border">
+            <p>Text Field</p>
+          </Container>
+        </Col>
+        <Col>
+          <Container className="container-border">
+            <p>Text Field</p>
+          </Container>
+        </Col>
+        <Col>
+          <Container className="container-border">
+            <p>Text Field</p>
+          </Container>
+        </Col>
+      </Row>
     </Container>
 
   </>
