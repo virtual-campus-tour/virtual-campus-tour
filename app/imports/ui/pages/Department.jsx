a664451ddee50d37fd8ba508988060e370ac1507
amandanitta <amandanitta458@gmail.com>
2022-11-25 15:09:52 -1000

    trying to add photos

--- app/imports/ui/pages/Department.jsx
+++ app/imports/ui/pages/Department.jsx
@@ -56,7 +56,14 @@ const Department = () => (
         </Col>
         <Col>
           <Container className="container-border">
-            <p>Text Field</p>
+            <div className="text-center">
+              <img src="/images/mamogram.jpg" alt="mamogram" className="box-img-size" />
+              <img src="/images/medical.jpg" alt="medical" className="box-img-size" />
+            </div>
+            <h4 className="text-center">Medical Imaging</h4>
+            <p className="text-center">Computer Science faculty Dr. Peter Washington and Dr. Peter Sadowski collaborated with Dr. John Shepherd at the UH Cancer Center to apply AI to medical imaging applications, including total-body DXA,breast cancer detection, and the Makawalu project which is developing a portable AI-based  ultrasound breast cancer screening tool.</p>
+            <img src="/images/peter-sadowski.jpg" alt="peter-sadowski" className="box-img-size" />
+            <img src="/images/peter-washington.jpg" alt="peter-washington" className="box-img-size" />
           </Container>
         </Col>
         <Col>
