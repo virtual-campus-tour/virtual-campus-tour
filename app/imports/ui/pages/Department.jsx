adbfca759271a5abcddf5b05346644283fa0ffca
amandanitta <amandanitta458@gmail.com>
2022-11-27 18:05:17 -1000

    updated

--- app/imports/ui/pages/Department.jsx
+++ app/imports/ui/pages/Department.jsx
@@ -53,6 +53,7 @@ const Department = () => (
               <img src="/images/lavaLabLogo200x200.jpg" alt="Lava Lab Logo" className="box-img-size" />
             </div>
             <h4 className="text-center">LAVA Lab</h4>
+            {/* eslint-disable-next-line max-len */}
             <p className="text-center">The LAVA Lab is a notable laboratory with Computer Science faculty, Directory Dr. Jason Leigh, that is pivotal in creating data visualization that assists in making predictions and preparing the State of Hawaii for the future with an emphasis on climate change</p>
           </Container>
         </Col>
@@ -65,6 +66,7 @@ const Department = () => (
               <img src="/images/peter-washington.jpg" alt="peter-washington" className="box-img-sizeP" />
             </div>
             <h4 className="text-center">Medical Imaging</h4>
+            {/* eslint-disable-next-line max-len */}
             <p className="text-center">Computer Science faculty Dr. Peter Washington and Dr. Peter Sadowski collaborated with Dr. John Shepherd at the UH Cancer Center to apply AI to medical imaging applications, including total-body DXA,breast cancer detection, and the Makawalu project which is developing a portable AI-based  ultrasound breast cancer screening tool.</p>
           </Container>
         </Col>
