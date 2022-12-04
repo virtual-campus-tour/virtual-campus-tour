31d89a3c6f691c5dc7a208368cc3b64be09acea2
amandanitta <amandanitta458@gmail.com>
2022-12-03 21:51:23 -1000

    draft 1

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col } from 'react-bootstrap';
+import { Col, Container } from 'react-bootstrap';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => {
   Meteor.logout();
   return (
-    <Col id="signout-page" className="text-center py-3"><h2>You are signed out.</h2></Col>
+    <Container id="signout-page">
+      <Col className="text-center py-3">
+        <h2>Thank you for visiting the University of Hawaii at Manoa Virtually</h2>
+        <h3>We hope to see you in person soon!</h3>
+      </Col>
+      <Col>
+        <h1>Go Rainbows!</h1>
+      </Col>
+    </Container>
   );
 };
 
