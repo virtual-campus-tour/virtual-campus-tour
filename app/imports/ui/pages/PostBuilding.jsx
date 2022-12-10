a9c4633a0ce139ef63c85969f37fb2709a14f572
Derek Nishimura <nishimuradj@gmail.com>
2022-12-10 11:19:11 -1000

    Fixed POST Page

--- app/imports/ui/pages/PostBuilding.jsx
+++ app/imports/ui/pages/PostBuilding.jsx
@@ -22,30 +22,32 @@ const PostBuilding = () => (
         <p className="text-center-p">
           As a leading campus in STEM, our Pacific, Ocean, Science and Technology building, commonly referred to as POST. Features our Hawai’i Space Flight laboratory and planetarium.
         </p>
-        <Row>
-          <Col>
-            <div className="small-spacer" />
-            <p className="text-center-p">
-              Our CyberCANOE data visualization system in Keller Hall can quickly analyze and interpret massive amounts of data collected by oceanographers and astronomers.
-              With new engineering science degrees offered in concentration focuses in biomedical and aeronautical engineering, our STEM programs are providing opportunities of the next generation.
-            </p>
-          </Col>
-          <Col>
-            <img className="image-fill general-image centered" src="/images/cyberCanoe.png" alt="CyberCanoe" />
-          </Col>
-        </Row>
-        <p className="text-center-p" />
-        <Row>
-          <Col>
-            <img className="image-fill general-image centered" src="/images/ICSpace_photo.png" alt="ICSpace" />
-          </Col>
-          <Col>
-            <p className="text-center-p">
-              Within the POST building on the third floor is ICSpace, known as a lounge for Computer Science students, where club meetings and leisure/ study time between classes occur.
-              In this space, there are servers maintained by the cybersecurity club along with white boards and tables for effective studying.
-            </p>
-          </Col>
-        </Row>
+        <Container className="small-spacer">
+          <Row>
+            <Col>
+              <p className="text-center-p tiny-spacer">
+                Our CyberCANOE data visualization system in Keller Hall can quickly analyze and interpret massive amounts of data collected by oceanographers and astronomers.
+                With new engineering science degrees offered in concentration focuses in biomedical and aeronautical engineering, our STEM programs are providing opportunities of the next generation.
+              </p>
+            </Col>
+            <Col>
+              <img className="image-fill general-image centered" src="/images/cyberCanoe.png" alt="CyberCanoe" />
+            </Col>
+          </Row>
+        </Container>
+        <Container className="small-spacer small-spacer-bottom">
+          <Row>
+            <Col>
+              <img className="image-fill general-image centered" src="/images/ICSpace_photo.png" alt="ICSpace" />
+            </Col>
+            <Col>
+              <p className="text-center-p small-spacer">
+                Within the POST building on the third floor is ICSpace, known as a lounge for Computer Science students, where club meetings and leisure/ study time between classes occur.
+                In this space, there are servers maintained by the cybersecurity club along with white boards and tables for effective studying.
+              </p>
+            </Col>
+          </Row>
+        </Container>
       </Col>
     </Container>
     <Row>
