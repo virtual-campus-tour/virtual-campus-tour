2e34c00dbcca57dc775fa97a31cb3e814b411499
amandanitta <amandanitta458@gmail.com>
2022-12-10 11:08:22 -1000

    updated post building page

--- app/imports/ui/pages/PostBuilding.jsx
+++ app/imports/ui/pages/PostBuilding.jsx
@@ -15,21 +15,37 @@ const PostBuilding = () => (
         </Row>
       </Container>
     </Row>
-    <Container className="small-spacer">
-      <h1>Pacific Ocean Science Technology Building</h1>
+    <Container className="center">
+      <div className="small-spacer" />
+      <h1>Pacific Ocean Science Technology Building (POST)</h1>
       <Col>
-        <p>
+        <p className="text-center-p">
           As a leading campus in STEM, our Pacific, Ocean, Science and Technology building, commonly referred to as POST. Features our Hawai’i Space Flight laboratory and planetarium.
         </p>
-        <p>
-          Our CyberCANOE data visualization system in Keller Hall can quickly analyze and interpret massive amounts of data collected by oceanographers and astronomers.
-        </p>
-        <p>
-          Within Holmes Hall our College of Engineering is developing our future leaders within the disciplines of civil, computer, electrical, and mechanical as the only accredited engineering programs offered in the state of Hawaii by ABET.
-        </p>
-        <p>
-          With new engineering science degrees offered in concentration focuses in biomedical and aeronautical engineering, our STEM programs are providing opportunities of the next generation.
-        </p>
+        <Row>
+          <Col>
+            <div className="small-spacer" />
+            <p className="text-center-p">
+              Our CyberCANOE data visualization system in Keller Hall can quickly analyze and interpret massive amounts of data collected by oceanographers and astronomers.
+              With new engineering science degrees offered in concentration focuses in biomedical and aeronautical engineering, our STEM programs are providing opportunities of the next generation.
+            </p>
+          </Col>
+          <Col>
+            <img className="image-fill general-image centered" src="/images/cyberCanoe.png" alt="CyberCanoe" />
+          </Col>
+        </Row>
+        <p className="text-center-p" />
+        <Row>
+          <Col>
+            <img className="image-fill general-image centered" src="/images/ICSpace_photo.png" alt="ICSpace" />
+          </Col>
+          <Col>
+            <p className="text-center-p">
+              Within the POST building on the third floor is ICSpace, known as a lounge for Computer Science students, where club meetings and leisure/ study time between classes occur.
+              In this space, there are servers maintained by the cybersecurity club along with white boards and tables for effective studying.
+            </p>
+          </Col>
+        </Row>
       </Col>
     </Container>
     <Row>
